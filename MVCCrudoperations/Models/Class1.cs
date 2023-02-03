82923fc427e073a0ca21276a68882e9ef4260f85
Deepak BOORLA <deepakb.in@mouritech.com>
2023-02-03 16:11:02 +0530

    created a class1

--- MVCCrudoperations/Models/Class1.cs
+++ MVCCrudoperations/Models/Class1.cs
@@ -6,9 +6,12 @@ using System.Web;
 namespace MVCCrudoperations.Models
 {
     public class Class1
-    {
+    { 
         public int Id { get; set; }
         public string Name { get; set; }
+            
         public string Description { get; set; }
+            
+        public int Age { get; set; }
     }
 }
